66c4eed10461661c9cc314fae327ed1faa57a474
abelang15 <abela15@lsu.edu>
2023-08-19 16:19:36 -0500

    these is the doctored up graph/code

--- code/temperature_index_plot.R
+++ code/temperature_index_plot.R
@@ -5,6 +5,11 @@ read_csv("data/GLB.Ts+dSST.csv", skip=1, na = "***") %>%
   select(year = Year, t_diff = 'J-D') %>%
   ggplot(aes(x=year, y=t_diff)) +
   geom_line(color = "gray") +
-  geom_point(fill= "white", color="gray")
+  geom_point(fill= "white", color="gray", shape=21) +
+  theme_light() +
+  geom_smooth(se= FALSE, color= "black", size=0.5, span=0.25)
+  
+  
+  
   
 
